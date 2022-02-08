c2aced6a7f66c7b18de553d57c37c13b8d062a10
pnaatz <41494483+pnaatz@users.noreply.github.com>
2022-02-07 20:21:41 -0500

    Btn Toggling
    Made the buttons toggle

---
+++ Recording.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Schillinger_Quest4_ErrorInputDatabase
+{
+    partial class Recording
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.recordBtn = new System.Windows.Forms.Button();
+            this.stopRecordingBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // recordBtn
+            // 
+            this.recordBtn.Location = new System.Drawing.Point(77, 153);
+            this.recordBtn.Name = "recordBtn";
+            this.recordBtn.Size = new System.Drawing.Size(139, 63);
+            this.recordBtn.TabIndex = 0;
+            this.recordBtn.Text = "Record";
+            this.recordBtn.UseVisualStyleBackColor = true;
+            this.recordBtn.Click += new System.EventHandler(this.recordBtn_Click);
+            // 
+            // stopRecordingBtn
+            // 
+            this.stopRecordingBtn.Location = new System.Drawing.Point(77, 222);
+            this.stopRecordingBtn.Name = "stopRecordingBtn";
+            this.stopRecordingBtn.Size = new System.Drawing.Size(139, 63);
+            this.stopRecordingBtn.TabIndex = 1;
+            this.stopRecordingBtn.Text = "Stop Recording";
+            this.stopRecordingBtn.UseVisualStyleBackColor = true;
+            this.stopRecordingBtn.Click += new System.EventHandler(this.stopRecordingBtn_Click);
+            // 
+            // Recording
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stopRecordingBtn);
+            this.Controls.Add(this.recordBtn);
+            this.Name = "Recording";
+            this.Text = "Recording";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button recordBtn;
+        private System.Windows.Forms.Button stopRecordingBtn;
+    }
+}
---
+++ Recording.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gma.UserActivityMonitor;
+
+namespace Schillinger_Quest4_ErrorInputDatabase
+{
+    public partial class Recording : Form
+    {
+        #region fields
+        XML xmlFile = new XML();
+        XML session;
+
+        Timer timer;
+        int count = 0;
+
+        string bugName = "bug";
+
+        XML lastEvent = null;
+        #endregion
+
+        #region Properties
+        XML Event
+        {
+            get
+            {
+                if (lastEvent != null)
+                {
+                    return lastEvent;
+                }
+
+                lastEvent = getNewEventXML();
+                return lastEvent;
+            }
+        }
+
+        XML MouseEvent
+        {
+            get
+            {
+                return Event.AddChild("Mouse");
+            }
+        }
+
+        XML KeyboardEvent
+        {
+            get
+            {
+                return Event.AddChild("Keyboard");
+            }
+        }
+        #endregion
+
+        enum Status
+        {
+            Not_Recording,
+            Recording
+        }
+
+        Status status = Status.Not_Recording;
+
+        public Recording()
+        {
+            InitializeComponent();
+            stopRecordingBtn.Enabled = false;
+            stopRecordingBtn.Visible = false;
+        }
+
+        private void stopRecordingBtn_Click(object sender, EventArgs e)
+        {
+            status = Status.Not_Recording;
+            ToggleButtons();
+        }
+
+        private void recordBtn_Click(object sender, EventArgs e)
+        {
+            status = Status.Recording;
+            ToggleButtons();
+        }
+
+        private void ToggleButtons()
+        {
+            ToggleButton(recordBtn);
+            ToggleButton(stopRecordingBtn);
+        }
+
+        private void ToggleButton(Button btn)
+        {
+            btn.Enabled = !btn.Enabled;
+            btn.Visible = !btn.Visible;
+        }
+
+
+        #region Helper Methods
+        XML getNewEventXML()
+        {
+            timer.Stop();
+            XML eventXML = session.AddChild("Event");
+            eventXML.AddAttribute("Time", count.ToString());
+
+            count = 0;
+            timer.Start();
+
+            return eventXML;
+        }
+
+        private void Count(object sender, EventArgs e)
+        {
+            count++;
+            lastEvent = null;
+        }
+        #endregion
+
+        #region Events
+        #region Form Events
+        private void Recording_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Recording_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            xmlFile.ExportXML(bugName + ".XML");
+        }
+        #endregion
+
+        #region Mouse Events
+        private void MouseMove(object sender, MouseEventArgs e)
+        {
+            XML mouseEvent = MouseEvent;
+            mouseEvent.AddAttribute("Position", e.Location.ToString());
+        }
+
+        //Mouse click removed because we have the up and down functions to track it
+        //private void MouseClick(object sender, MouseEventArgs e)
+        //{
+        //    XML mouseEvent = MouseEvent;
+        //    mouseEvent.AddAttribute("Click", e.Button.ToString());
+        //}
+        private void MouseUp(object sender, MouseEventArgs e)
+        {
+            XML mouseEvent = MouseEvent;
+            mouseEvent.AddAttribute(e.Button.ToString(), "Up");
+        }
+
+        private void MouseDown(object sender, MouseEventArgs e)
+        {
+            XML mouseEvent = MouseEvent;
+            mouseEvent.AddAttribute(e.Button.ToString(), "Down");
+        }
+
+        private void MouseWheelMove(object sender, MouseEventArgs e)
+        {
+            XML mouseEvent = MouseEvent;
+            mouseEvent.AddAttribute("Scroll", e.Delta.ToString());
+        }
+        #endregion
+
+        #region KeyBoard Event
+        private void KeyUp(object sender, KeyEventArgs e)
+        {
+            XML keyBoardEvent = KeyboardEvent;
+            keyBoardEvent.AddAttribute(e.KeyCode.ToString(), "Up");
+        }
+
+        private void KeyDown(object sender, KeyEventArgs e)
+        {
+            XML keyBoardEvent = KeyboardEvent;
+            keyBoardEvent.AddAttribute(e.KeyCode.ToString(), "Down");
+        }
+        #endregion
+
+        #region Event Helper Methods
+        private void AddEvents()
+        {
+            HookManager.MouseUp += MouseUp;
+            HookManager.MouseDown += MouseDown;
+            HookManager.MouseMove += MouseMove;
+            HookManager.KeyUp += KeyUp;
+            HookManager.KeyDown += KeyDown;
+            HookManager.MouseWheel += MouseWheelMove;
+        }
+
+        private void RemoveEvents()
+        {
+            HookManager.MouseUp -= MouseUp;
+            HookManager.MouseDown -= MouseDown;
+            HookManager.MouseMove -= MouseMove;
+            HookManager.KeyUp -= KeyUp;
+            HookManager.KeyDown -= KeyDown;
+            HookManager.MouseWheel -= MouseWheelMove;
+        }
+        #endregion
+
+        #endregion
+
+    }
+}
